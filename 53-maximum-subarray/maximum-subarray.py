fba8d763c3f3e8c4139b3bc08a9efe054413f605
CHETHAN PATEL <87313707+Chethanpatel@users.noreply.github.com>
2025-08-13 13:58:04 +0530

    Time: 91 ms (28.95%) | Memory: 32.8 MB (21.96%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curSum = 0
+        maxSum = nums[0]
+        for num in nums:
+            curSum = max(curSum, 0)
+            curSum += num
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
+
+## The idea is we should make sure that current sum is not negative, as soon as it is negative, set it to zero. at the same time,
+## upadte the maximum sum maxumm of surrent sum and already existing maxmum sum. Snippet 
+# curSum += num tells upadte the cursum 
